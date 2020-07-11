e620d69b54629566050f976b2ce18c601c306338
w0xter <luispozogilo@gmail.com>
2020-07-11 19:17:55 +0200

    Rebuilding the project using swagger

--- app/controllers/UserController.py
+++
@@ -1,6 +0,0 @@
-
-class UserController:
-    def __init__(db):
-        self.db = db
-    def get_user(id):
-        db.
--- app/models/User.py
+++
@@ -1,8 +0,0 @@
-from flask_sqlalchemy import SQLAlchemy
-
-class User(db.Model):
-    id = db.column('student_id', db.Integer, primary_key=True) 
-    usesrname = db.column(db.String(100))
-    password = db.column(db.String(100))
-    email = db.column(db.String(100))
-    deleted = db.column(db.Boolean)
